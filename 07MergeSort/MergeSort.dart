1814ea6f5fd55a24f9280a5eab27c4a243171d02
tianqing.liang <1>
2021-08-06 17:04:17 +0800

    归并排序

---
+++ 07MergeSort/MergeSort.dart
@@ -0,0 +1,50 @@
+import 'ArrayGenerator.dart';
+
+/**
+ * 归并排序
+ */
+class MergeSort{
+
+  static sort(List arr){
+    _sortContent(arr,0,arr.length-1);
+  }
+
+  static _sortContent (List arr,int left,int right){
+    if(left >= right){
+      return;
+    }
+    int mid = ((right-left)/2).toInt() +left;
+    _sortContent(arr, left, mid);
+    _sortContent(arr, mid+1, right);
+    _merge(arr,left,mid,right);
+  }
+
+  static _merge(List arr,int l,int mid,int right){
+//    List temp = List.copyRange(arr, l, r!+1);
+    List temp = List.filled(right+1-l, true);
+    List.copyRange(temp, 0, arr,l,right+1);
+    int i = l, j = mid + 1;
+
+    // 每轮循环为 arr[k] 赋值
+    for(int k = l; k <= right; k ++){
+      if(i > mid){
+        arr[k] = temp[j - l]; j ++;
+      }
+      else if(j > right){
+        arr[k] = temp[i - l]; i ++;
+      }
+      else if(temp[i - l].compareTo(temp[j - l]) <= 0){
+        arr[k] = temp[i - l]; i ++;
+      }
+      else{
+        arr[k] = temp[j - l]; j ++;
+      }
+    }
+  }
+}
+void main(){
+  int n = 100;
+  List arr = ArrayGenerator.generateRandomArray(n, n);
+  MergeSort.sort(arr);
+  print(arr);
+}
